8951d7bc70e81ade6757252f61f856749a7c6be2
Bakhtawar-Rashid <Bakhtawarashid@gmail.com>
2022-05-31 03:26:47 +0500

    image-round

--- login.php
+++ login.php
@@ -10,12 +10,13 @@ $title = 'Login | Page';
     <title><?=$title?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/5.2/examples/sign-in/signin.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="text-center">
     
 <main class="form-signin w-100 m-auto">
   <form>
-    <img class="mb-4" src="https://picsum.photos/72/57" alt="" width="72" height="57">
+    <img class="mb-4" src="https://picsum.photos/72" alt="" width="72" height="72">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
--- register.php
+++ register.php
@@ -11,12 +11,13 @@ include 'db/connect.php';
     <title><?=$tile?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/5.2/examples/sign-in/signin.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="text-center">
     
 <main class="form-signin w-100 m-auto">
   <form action="" method="POST">
-    <img class="mb-4" src="https://picsum.photos/72/57" alt="" width="72" height="57">
+    <img class="mb-4 img-circle" src="https://picsum.photos/72" alt="" width="72" height="72">
     <h1 class="h3 mb-3 fw-normal"><?=(isset($user) ? $user:'Please sign up')?></h1>
     <h5>
       <?php
@@ -40,7 +41,7 @@ include 'db/connect.php';
       <input type="text"  name="username" class="form-control" id="floatingInput" placeholder="username">
       <label for="floatingInput">Username</label>
     </div>
-    <div class="form-floating">
+    <div class="form-floating my-3">
       <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
@@ -50,7 +51,7 @@ include 'db/connect.php';
     </div>
 
     <button class="w-100 btn btn-lg btn-primary" name="sub_btn" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-2022</p>
+    <p class="mt-5 mb-3 text-muted">&copy; <?= date('D-M-Y'); ?></p>
   </form>
 </main>
     
