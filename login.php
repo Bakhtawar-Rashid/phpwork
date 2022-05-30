91f7efade37a9b2e005ba29c6737ea910ef8332d
SaadiDK-003 <pcremote059@gmail.com>
2022-05-31 02:07:49 +0500

    db ~ conn + register page work

--- login.php
+++ login.php
@@ -1,10 +1,13 @@
+<?php
+$title = 'Login | Page';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$title?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/5.2/examples/sign-in/signin.css">
 </head>
@@ -12,7 +15,7 @@
     
 <main class="form-signin w-100 m-auto">
   <form>
-    <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <img class="mb-4" src="https://picsum.photos/72/57" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
--- register.php
+++ register.php
@@ -1,10 +1,14 @@
+<?php
+$tile = 'Register | Page';
+include 'db/connect.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$tile?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/5.2/examples/sign-in/signin.css">
 </head>
@@ -12,9 +16,26 @@
     
 <main class="form-signin w-100 m-auto">
   <form action="" method="POST">
-    <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+    <img class="mb-4" src="https://picsum.photos/72/57" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal"><?=(isset($user) ? $user:'Please sign up')?></h1>
+    <h5>
+      <?php
+      if(isset($_POST['sub_btn'])){
 
+        $user = $_POST['username'];
+        $email = $_POST['email'];
+        $pwd = $_POST['password'];
+        $query = $db->query("INSERT INTO `users` (username,email,password) VALUES('$user','$email','$pwd')");
+        if($query){
+          echo 'User Registered Successfully!
+          <script>setTimeout(function(){
+            window.location.href="login.php";
+          },2800)</script>
+          ';
+        }
+    }
+      ?>
+    </h5>
     <div class="form-floating">
       <input type="text"  name="username" class="form-control" id="floatingInput" placeholder="username">
       <label for="floatingInput">Username</label>
@@ -28,7 +49,7 @@
       <label for="floatingPassword">Password</label>
     </div>
 
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <button class="w-100 btn btn-lg btn-primary" name="sub_btn" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-muted">&copy; 2017-2022</p>
   </form>
 </main>
